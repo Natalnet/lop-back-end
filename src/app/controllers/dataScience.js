dfbc011d41b73f8b6e5483f9ec252451cd65819a
hewerton <hewerton80@gmail.com>
2020-10-01 21:19:10 -0300

    atualiza endpoints para o consumo do dataScience

--- src/app/controllers/dataScience.js
+++ src/app/controllers/dataScience.js
@@ -1,35 +1,35 @@
 
 const path = require('path')
-const {Op} = require('sequelize')
+const { Op } = require('sequelize')
 
 const sequelize = require('../../database/connection')
-const {Submission,Question,ClassHasListQuestion,Class, User} = sequelize.import(path.resolve(__dirname,'..','models'))
+const { Submission, Question, ClassHasListQuestion, ListQuestions, Test, Class, User } = sequelize.import(path.resolve(__dirname, '..', 'models'))
 
-class CSVController{
-    async getCsvListClass(req,res){
-		const {idClass} = req.params;
-		try{
+class CSVController {
+	async getCsvListClass(req, res) {
+		const { idClass } = req.params;
+		try {
 			const classRoon = await Class.findByPk(idClass);
 			let users = await classRoon.getUsers({
-				where:{
+				where: {
 					profile: 'ALUNO',
 				},
-				order:['name'],
-				attributes: ['id','name','email'],
-				
+				order: ['name'],
+				attributes: ['id', 'name', 'email'],
+
 			});
 			let lists = await classRoon.getLists({
-				attributes:['id','title'],
-				order:[
-					['questions','createdAt']
+				attributes: ['id', 'title'],
+				order: [
+					['questions', 'createdAt']
 				],
-				include:[{
-					model:Question,
-					as:'questions',
-					attributes:['id']
+				include: [{
+					model: Question,
+					as: 'questions',
+					attributes: ['id']
 				}],
 			});
-			users = users.map(user=>{
+			users = users.map(user => {
 				const userCopy = JSON.parse(JSON.stringify(user));
 				userCopy.enrollment = userCopy.classHasUser.enrollment;
 				delete userCopy.classHasUser;
@@ -37,31 +37,31 @@ class CSVController{
 			})
 			users = await Promise.all(users.map(async user => {
 				lists = await Promise.all(lists.map(async list => {
-					const classHasListQuestion =  await ClassHasListQuestion.findOne({
-						where:{
-							list_id : list.id,
+					const classHasListQuestion = await ClassHasListQuestion.findOne({
+						where: {
+							list_id: list.id,
 							class_id: idClass
 						},
-						attributes:['createdAt','submissionDeadline']
+						attributes: ['createdAt', 'submissionDeadline']
 					})
 					let submissionDeadline = classHasListQuestion.submissionDeadline
-					submissionDeadline = submissionDeadline?new Date(submissionDeadline):null;
-					const questions = await Promise.all(list.questions.map(async question=>{
+					submissionDeadline = submissionDeadline ? new Date(submissionDeadline) : null;
+					const questions = await Promise.all(list.questions.map(async question => {
 						const query = {
-							where:{
-								user_id     : user.id,
-								question_id : question.id,
-								listQuestions_id : list.id,
-								class_id         : idClass
+							where: {
+								user_id: user.id,
+								question_id: question.id,
+								listQuestions_id: list.id,
+								class_id: idClass
 							}
 						}
-		
+
 						const submissionsCount = await Submission.count(query)
 						query.where.hitPercentage = 100
-						const correctSumissionsCount  = await Submission.count(query)
-						if(submissionDeadline){
+						const correctSumissionsCount = await Submission.count(query)
+						if (submissionDeadline) {
 							query.where.createdAt = {
-								[Op.lte] : submissionDeadline
+								[Op.lte]: submissionDeadline
 							}
 						}
 						const completedSumissionsCount = await Submission.count(query)
@@ -69,13 +69,13 @@ class CSVController{
 						delete questionCopy.listHasQuestion
 						questionCopy.completedSumissionsCount = completedSumissionsCount
 						questionCopy.submissionsCount = submissionsCount
-						questionCopy.isCorrect = correctSumissionsCount>0
+						questionCopy.isCorrect = correctSumissionsCount > 0
 						return questionCopy
 					}))
 					const listCopy = JSON.parse(JSON.stringify(list))
-			
+
 					listCopy.questionsCount = questions.length
-					listCopy.questionsCompletedSumissionsCount = questions.filter(q=>q.completedSumissionsCount>0).length
+					listCopy.questionsCompletedSumissionsCount = questions.filter(q => q.completedSumissionsCount > 0).length
 					delete listCopy.questions;
 					listCopy.classHasListQuestion = classHasListQuestion;
 					return listCopy;
@@ -84,54 +84,72 @@ class CSVController{
 				userCopy.lists = lists;
 				return userCopy;
 			}))
-		
-	
+
+
 			return res.status(200).json(users)
 		}
-		catch(err){
+		catch (err) {
 			console.log(err)
 			return res.status(500).json(err)
 		}
-    }	
-    
-    async getCsvSubmissionClass(req,res){
-        const { idClass } = req.params;
-		try{
-			let submissions =  await Submission.findAll({
-				where:{
-                    class_id : idClass
-                },
+	}
+
+	async getCsvSubmissionClass(req, res) {
+		const { idClass } = req.params;
+		try {
+			let submissions = await Submission.findAll({
+				where: {
+					class_id: idClass
+				},
 				order: [
-					['createdAt','DESC']
-                ],
-                attributes: ['id','ip','environment','language','char_change_number','timeConsuming','createdAt','listQuestions_id','test_id','question_id'],
-				include:[{
-					model:User,
-					as:'user',
-					where:{
-						profile:"ALUNO",
+					['createdAt', 'DESC']
+				],
+				attributes: ['id', 'ip', 'environment', 'hitPercentage', 'language', 'char_change_number', 'timeConsuming', 'createdAt', 'listQuestions_id', 'test_id', 'question_id'],
+				include: [{
+					model: User,
+					as: 'user',
+					where: {
+						profile: "ALUNO",
 					},
-                    attributes:['id','name','email'],
-                    include: [{
-                        model: Class,
-                        as: 'classes',
-                        where: {
-                            id: idClass
-                        },
-                        attributes: ['id']
-                    }]
+					attributes: ['id', 'name', 'email'],
+					include: [{
+						model: Class,
+						as: 'classes',
+						where: {
+							id: idClass
+						},
+						attributes: ['id']
+					}]
+				}, {
+					model: Question,
+					as: 'question',
+					attributes: ['title']
+				}, {
+					model: ListQuestions,
+					as: 'list',
+					attributes: ['title']
+				},{
+					model: Test,
+					as: 'test',
+					attributes: ['title']
 				}]
-            })
-            submissions = submissions.map(submission => {
-                const sucmissionCopy = JSON.parse(JSON.stringify(submission))
-                sucmissionCopy.user.enrollment = submission.user.classes[0].classHasUser.enrollment;
-                delete sucmissionCopy.user.classes
-                return sucmissionCopy
-            })
-			
+			})
+			submissions = submissions.map(submission => {
+				const submissionCopy = JSON.parse(JSON.stringify(submission))
+				submissionCopy.user = submission.user.classes[0].classHasUser.enrollment;
+				submissionCopy.question = submission.question.title;
+				submissionCopy.list = submission.list && submission.list.title;
+				submissionCopy.test = submission.test && submission.test.title;
+				delete submissionCopy.question_id;
+				delete submissionCopy.listQuestions_id;
+				delete submissionCopy.user.classes;
+				delete submissionCopy.test_id;
+				return submissionCopy;
+			})
+
 			return res.status(200).json(submissions);
 		}
-		catch(err){
+		catch (err) {
 			console.log(err);
 			return res.status(500).json(err)
 		}
