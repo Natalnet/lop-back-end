d05eb8fdf8f5c16b66e5d2501b882a9c60d13c0c
hewerton <hewerton80@gmail.com>
2020-12-08 20:28:48 -0300

    adiciona endpoint para obter todas as listas de uma turma

--- src/routes/course.js
+++ src/routes/course.js
@@ -5,6 +5,7 @@ const CourseController = require('../app/controllers/courseController')
 router.use('/course',AuthMiddleware.authentication)
 
 router.get('/course/page/:page',CourseController.getCourses);
+router.get('/course/class/:id/page/:page',CourseController.getCoursesByClass);
 router.get('/course/:id',CourseController.getCourse);
 router.post('/course',CourseController.createCourse);
 router.put('/course/:id',CourseController.updateCourse);
