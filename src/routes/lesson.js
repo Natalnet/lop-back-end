35fd70e7757678eed759cdcbb5cbd233243723fd
hewerton <hewerton80@gmail.com>
2021-01-28 09:02:05 -0300

    descomenta tudo que é de lesson e course

--- src/routes/lesson.js
+++ src/routes/lesson.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const AuthMiddleware = require('../app/middlewares/authMiddleware')
-// const LessonController = require('../app/controllers/lessonController')
+const LessonController = require('../app/controllers/lessonController')
 
 router.use('/lesson',AuthMiddleware.authentication)
 
-// router.get('/lesson/course/:idCourse',LessonController.getLessonsByCourse);
-// router.get('/lesson/:id',LessonController.getLesson);
-// router.post('/lesson',LessonController.createLesson);
-// router.put('/lesson/:id',LessonController.updateLesson);
-// router.put('/lesson/:id/visibility',LessonController.updateVisibilityLesson);
+router.get('/lesson/course/:idCourse',LessonController.getLessonsByCourse);
+router.get('/lesson/:id',LessonController.getLesson);
+router.post('/lesson',LessonController.createLesson);
+router.put('/lesson/:id',LessonController.updateLesson);
+router.put('/lesson/:id/visibility',LessonController.updateVisibilityLesson);
 
 module.exports = (app) => app.use(router);
