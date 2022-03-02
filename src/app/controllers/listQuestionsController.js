b535eca8b857e3250388499be72b24a5551b3dd5
Wendell P. Barreto <wendellp.barreto@gmail.com>
2022-03-02 19:56:37 -0300

    improvement: enhance performance of list class questions endpoint

--- src/app/controllers/listQuestionsController.js
+++ src/app/controllers/listQuestionsController.js
@@ -1,209 +1,214 @@
-const crypto = require('crypto');
-const {Op, fn, col} = require('sequelize')
-const ListQuestionsService = require('../services/listQuestionsService');
-const sequelize = require('../../database/connection');
-const { resolve } = require('path');
-const {ListQuestions,Question,User} = sequelize.import(resolve(__dirname,'..','models'))
+const crypto = require("crypto");
+const { Op, fn, col } = require("sequelize");
+const ListQuestionsService = require("../services/listQuestionsService");
+const sequelize = require("../../database/connection");
+const { resolve } = require("path");
+const { ListQuestions, Question, User } = sequelize.import(
+  resolve(__dirname, "..", "models")
+);
 
-class ListQuestionsController{
-	async index(req,res){
-		const idUser = req.query.idUser || req.userId
-		const idClass = req.query.idClass
-		try{
+class ListQuestionsController {
+  async index(req, res) {
+    const idUser = req.query.idUser || req.userId;
+    const idClass = req.query.idClass;
 
-			let lists = await ListQuestionsService.getListsFromClassWithQuestionsAndUserInfos(idClass, idUser)
-	
-			if(!lists){
-				return res.status(500).json(err)
-			}
-			return res.status(200).json(lists)
-		}
-		catch(err){
-			console.log(err)
-			return res.status(500).json(err)
-		}
-	}
-	async index_paginate(req,res){
-		const idNotInt = req.query.idNotInt || ''
-		const include = req.query.include || ''
-		const field = req.query.field || 'title'
-		//const sort = req.query.sort || '-createdAt'
-		
-		const limitDocsPerPage=parseInt(req.query.docsPerPage || 15);
-		let page = parseInt(req.params.page || 1);
-		try{
-			const listQuestions = {}
-			const query = {
-				where : {
-					id:{
-						[Op.notIn]:idNotInt.split(' ')
-					},
-					title: { 
-						[Op.like]: `%${field==='title'?include:''}%` 
-					},
-					code: { 
-						[Op.like]: `%${field==='code'?include:''}%` 
-					}
-				},
-				order: [
-					['createdAt','DESC']
-				],
+    try {
+      const lists =
+        await ListQuestionsService.getListsFromClassWithUserSubmissions(
+          idClass,
+          idUser
+        );
 
-			}
-			 
+      return res.status(200).json(lists);
+    } catch (err) {
+      console.log(err);
 
-			listQuestions.count = await ListQuestions.count(query)
+      return res.status(500).json(err);
+    }
+  }
 
-			const totalPages = Math.ceil(listQuestions.count/limitDocsPerPage)
-			//console.log('total ',listQuestions.count)
-			page = parseInt(page>totalPages?totalPages:page)
-			page = page<=0?1:page
+  async index_paginate(req, res) {
+    const idNotInt = req.query.idNotInt || "";
+    const include = req.query.include || "";
+    const field = req.query.field || "title";
+    //const sort = req.query.sort || '-createdAt'
 
-			query.limit = limitDocsPerPage
-			query.offset = (page-1)*limitDocsPerPage
-			query.include = [{
-				model : Question,
-				as    : 'questions',
-			},{
-				model: User,
-				as: "author",
-				attributes:["email"]
-			}]
-			listQuestions.rows = await ListQuestions.findAll(query)
+    const limitDocsPerPage = parseInt(req.query.docsPerPage || 15);
+    let page = parseInt(req.params.page || 1);
+    try {
+      const listQuestions = {};
+      const query = {
+        where: {
+          id: {
+            [Op.notIn]: idNotInt.split(" "),
+          },
+          title: {
+            [Op.like]: `%${field === "title" ? include : ""}%`,
+          },
+          code: {
+            [Op.like]: `%${field === "code" ? include : ""}%`,
+          },
+        },
+        order: [["createdAt", "DESC"]],
+      };
 
-			const listQuestionsPaginate = {
-				docs        : listQuestions.rows,
-				currentPage : page,
-				perPage     : parseInt(limitDocsPerPage),
-				total       : parseInt(listQuestions.count),
-				totalPages  : parseInt(totalPages)
-			}
-			return res.status(200).json(listQuestionsPaginate)
-		}
-		catch(err){
-			console.log(err);
-			return res.status(500).json(err)
-		}
-	}
+      listQuestions.count = await ListQuestions.count(query);
 
-	async getCountLists(req, res){
-		try{
-			const countLists = await ListQuestions.count();
-			return res.status(200).json({ countLists})
-		}
-		catch(err){
-			console.log(err);
-			return res.status(500).json(err)
-		}
-	}
+      const totalPages = Math.ceil(listQuestions.count / limitDocsPerPage);
+      //console.log('total ',listQuestions.count)
+      page = parseInt(page > totalPages ? totalPages : page);
+      page = page <= 0 ? 1 : page;
 
-	async show(req,res){
-		const idList = req.params.id;
-		const idClass = req.query.idClass;
-		const idUser = req.query.idUser || req.userId;
-		try{
-			const userPromise = User.findOne({
-				where:{
-					id:idUser
-				},
-				attributes:['id','name'],
-			})
-			let listPromise = ListQuestionsService.getListFromClassWithQuestionsAndUserInfos(idList, idClass, idUser);
-			let [list,user] = await Promise.all([
-				listPromise,
-				userPromise
-			])
-			if(!list){
-				return res.status(500).json(err)
-			}
-			const response = req.query.idUser?{list,user}:list
-			
-			return res.status(200).json(response)
-		}
-		catch(err){
-			console.log(err)
-			return res.status(500).json(err)
-		}
-	}
+      query.limit = limitDocsPerPage;
+      query.offset = (page - 1) * limitDocsPerPage;
+      query.include = [
+        {
+          model: Question,
+          as: "questions",
+        },
+        {
+          model: User,
+          as: "author",
+          attributes: ["email"],
+        },
+      ];
+      listQuestions.rows = await ListQuestions.findAll(query);
 
-	async store(req,res){
-		try{
-			const {title,questions} = req.body
-			const code = crypto.randomBytes(5).toString('hex')
-			const listQuestion = await ListQuestions.create({
-				title,
-				code,
-				author_id:req.userId
-			})
-			const bulkQuestions = await Promise.all([...questions].map(async qId =>Question.findByPk(qId) ))
-			if(bulkQuestions.length>0){
-				await listQuestion.addQuestions(bulkQuestions)
-			}
-			//await listQuestion.getQuestions()
-			return res.status(200).json({msg:'ok'})
-		}
-		catch(err){
-            if(err.name==='SequelizeUniqueConstraintError' || err.name === 'SequelizeValidationError'){
-                const validationsErros = ([...err.errors].map(erro=>{
-                    let erroType = {
-                        field:erro.path,
-                        message:erro.message,
+      const listQuestionsPaginate = {
+        docs: listQuestions.rows,
+        currentPage: page,
+        perPage: parseInt(limitDocsPerPage),
+        total: parseInt(listQuestions.count),
+        totalPages: parseInt(totalPages),
+      };
+      return res.status(200).json(listQuestionsPaginate);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
 
-                    }
-                    return erroType
-                }));
-                //console.log(validationsErros)
-                return res.status(400).json(validationsErros)
-            }
-            else{
-                console.log(err);
-                return res.status(500).json(err)
-            }
-		}
-	}
+  async getCountLists(req, res) {
+    try {
+      const countLists = await ListQuestions.count();
+      return res.status(200).json({ countLists });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
 
-	async update(req,res){
-		try{
-			const {title,questions} = req.body
-			const {id} = req.params;
-			const listQuestion = await ListQuestions.findByPk(id);
-			if(!listQuestion){
-				return res.status(404).json()
-			}
-			//console.log(listQuestion)
-			if(listQuestion.author_id !== req.userId){	
-				//console.log("Sem permissão")
-				return res.status(401).json({msg:"Sem permissão"})
-			}
-			await listQuestion.update({
-				title,
-			})
-			const bulkQuestions = await Promise.all([...questions].map(async qId => Question.findByPk(qId) ))
-			if(bulkQuestions.length > 0){
-				await listQuestion.setQuestions(bulkQuestions);
-			}
-			//await listQuestion.getQuestions()
-			return res.status(200).json({msg:'ok'})
-		}
-		catch(err){
-            if(err.name==='SequelizeUniqueConstraintError' || err.name === 'SequelizeValidationError'){
-                const validationsErros = ([...err.errors].map(erro=>{
-                    let erroType = {
-                        field:erro.path,
-                        message:erro.message,
-                        
-                    }
-                    return erroType
-                }));
-                //console.log(validationsErros)
-                return res.status(400).json(validationsErros);
-            }
-            else{
-                console.log(err);
-                return res.status(500).json(err);
-            }
-		}
-	}
+  async show(req, res) {
+    const idList = req.params.id;
+    const idClass = req.query.idClass;
+    const idUser = req.query.idUser || req.userId;
+    try {
+      const userPromise = User.findOne({
+        where: {
+          id: idUser,
+        },
+        attributes: ["id", "name"],
+      });
+      let listPromise =
+        ListQuestionsService.getListFromClassWithQuestionsAndUserInfos(
+          idList,
+          idClass,
+          idUser
+        );
+      let [list, user] = await Promise.all([listPromise, userPromise]);
+      if (!list) {
+        return res.status(500).json(err);
+      }
+      const response = req.query.idUser ? { list, user } : list;
 
+      return res.status(200).json(response);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+
+  async store(req, res) {
+    try {
+      const { title, questions } = req.body;
+      const code = crypto.randomBytes(5).toString("hex");
+      const listQuestion = await ListQuestions.create({
+        title,
+        code,
+        author_id: req.userId,
+      });
+      const bulkQuestions = await Promise.all(
+        [...questions].map(async (qId) => Question.findByPk(qId))
+      );
+      if (bulkQuestions.length > 0) {
+        await listQuestion.addQuestions(bulkQuestions);
+      }
+      //await listQuestion.getQuestions()
+      return res.status(200).json({ msg: "ok" });
+    } catch (err) {
+      if (
+        err.name === "SequelizeUniqueConstraintError" ||
+        err.name === "SequelizeValidationError"
+      ) {
+        const validationsErros = [...err.errors].map((erro) => {
+          let erroType = {
+            field: erro.path,
+            message: erro.message,
+          };
+          return erroType;
+        });
+        //console.log(validationsErros)
+        return res.status(400).json(validationsErros);
+      } else {
+        console.log(err);
+        return res.status(500).json(err);
+      }
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { title, questions } = req.body;
+      const { id } = req.params;
+      const listQuestion = await ListQuestions.findByPk(id);
+      if (!listQuestion) {
+        return res.status(404).json();
+      }
+      //console.log(listQuestion)
+      if (listQuestion.author_id !== req.userId) {
+        //console.log("Sem permissão")
+        return res.status(401).json({ msg: "Sem permissão" });
+      }
+      await listQuestion.update({
+        title,
+      });
+      const bulkQuestions = await Promise.all(
+        [...questions].map(async (qId) => Question.findByPk(qId))
+      );
+      if (bulkQuestions.length > 0) {
+        await listQuestion.setQuestions(bulkQuestions);
+      }
+      //await listQuestion.getQuestions()
+      return res.status(200).json({ msg: "ok" });
+    } catch (err) {
+      if (
+        err.name === "SequelizeUniqueConstraintError" ||
+        err.name === "SequelizeValidationError"
+      ) {
+        const validationsErros = [...err.errors].map((erro) => {
+          let erroType = {
+            field: erro.path,
+            message: erro.message,
+          };
+          return erroType;
+        });
+        //console.log(validationsErros)
+        return res.status(400).json(validationsErros);
+      } else {
+        console.log(err);
+        return res.status(500).json(err);
+      }
+    }
+  }
 }
-module.exports = new ListQuestionsController()
+module.exports = new ListQuestionsController();
--- src/app/services/listQuestionsService.js
+++ src/app/services/listQuestionsService.js
@@ -93,85 +93,102 @@ class ListQuestionsService {
     }
   }
 
-  async getListsFromClassWithQuestionsAndUserInfos(idClass, idUser) {
-    try {
-      let lists = await ListQuestions.findAll({
-        attributes: ["id", "title"],
-        include: [
-          {
-            model: Question,
-            as: "questions",
-            attributes: ["id"],
+  async getListsFromClassWithUserSubmissions(idClass, idUser) {
+    const listsQuery = {
+      attributes: ["id", "title"],
+      include: [
+        {
+          model: Question,
+          as: "questions",
+          attributes: ["id"],
+        },
+        {
+          model: Class,
+          as: "classes",
+          attributes: ["id"],
+          where: {
+            id: idClass,
           },
-          {
-            model: Class,
-            as: "classes",
-            attributes: ["id"],
-            where: {
-              id: idClass,
-            },
-          },
-        ],
+        },
+      ],
+    };
+    let lists = await ListQuestions.findAll(listsQuery);
+
+    const submissionsQuery = {
+      attributes: [
+        "id",
+        "question_id",
+        "class_id",
+        "hitPercentage",
+        "createdAt",
+      ],
+      where: {
+        user_id: idUser,
+        class_id: idClass,
+      },
+    };
+    const submissions = await Submission.findAll(submissionsQuery);
+
+    lists = lists.map((list) => {
+      let { createdAt, submissionDeadline } =
+        list.classes[0].classHasListQuestion;
+      const classHasListQuestion = { createdAt, submissionDeadline };
+
+      submissionDeadline = submissionDeadline
+        ? new Date(submissionDeadline)
+        : null;
+
+      const questions = list.questions.map((question) => {
+        const questionSubmissions = submissions.filter(
+          (submission) =>
+            submission.question_id == question.id &&
+            submission.class_id == idClass
+        );
+        const completedQuestionSubmissions = questionSubmissions.filter(
+          (submission) =>
+            submission.hitPercentage == 100 &&
+            (submissionDeadline
+              ? submission.createdAt < submissionDeadline
+              : true)
+        );
+
+        const submissionsCount = questionSubmissions.length;
+        const completedSumissionsCount = completedQuestionSubmissions.length;
+
+        const questionCopy = JSON.parse(JSON.stringify(question));
+
+        delete questionCopy.listHasQuestion;
+        delete questionCopy.id;
+
+        questionCopy.submissions = questionSubmissions;
+        questionCopy.submissionsCount = submissionsCount;
+        questionCopy.completedSumissionsCount = completedSumissionsCount;
+
+        return questionCopy;
       });
-      lists = await Promise.all(
-        lists.map(async (list) => {
-          let { createdAt, submissionDeadline } =
-            list.classes[0].classHasListQuestion;
-          const classHasListQuestion = { createdAt, submissionDeadline };
-          submissionDeadline = submissionDeadline
-            ? new Date(submissionDeadline)
-            : null;
 
-          const questions = await Promise.all(
-            list.questions.map(async (question) => {
-              const query = {
-                where: {
-                  user_id: idUser,
-                  question_id: question.id,
-                  listQuestions_id: list.id,
-                  class_id: idClass,
-                },
-              };
-              const submissionsCount = await Submission.count(query);
-              query.where.hitPercentage = 100;
-              if (submissionDeadline) {
-                query.where.createdAt = {
-                  [Op.lte]: submissionDeadline,
-                };
-              }
-              const completedSumissionsCount = await Submission.count(query);
+      const listCopy = JSON.parse(JSON.stringify(list));
 
-              const questionCopy = JSON.parse(JSON.stringify(question));
-              delete questionCopy.listHasQuestion;
-              delete questionCopy.id;
-              questionCopy.submissionsCount = submissionsCount;
-              questionCopy.completedSumissionsCount = completedSumissionsCount;
-              //questionCopy.
-              return questionCopy;
-            })
-          );
-          const listCopy = JSON.parse(JSON.stringify(list));
-          delete listCopy.classes;
-          delete listCopy.questions;
-          listCopy.questionsCount = questions.length;
-          listCopy.questionsCompletedSumissionsCount = questions.filter(
-            (q) => q.completedSumissionsCount > 0
-          ).length;
-          listCopy.classHasListQuestion = classHasListQuestion;
-          return listCopy;
-        })
-      );
+      delete listCopy.classes;
+      delete listCopy.questions;
 
-      lists = lists.sort((l1, l2) => {
-        const date1 = l1.classHasListQuestion.createdAt;
-        const date2 = l2.classHasListQuestion.createdAt;
-        return date1 > date2 ? 1 : -1;
-      });
-      return lists;
-    } catch (err) {
-      console.log(err);
-      return null;
-    }
+      listCopy.questionsCount = questions.length;
+      listCopy.questionsCompletedSumissionsCount = questions.filter(
+        (q) => q.completedSumissionsCount > 0
+      ).length;
+      listCopy.classHasListQuestion = classHasListQuestion;
+
+      return listCopy;
+    });
+
+    lists = lists.sort((l1, l2) => {
+      const date1 = l1.classHasListQuestion.createdAt;
+      const date2 = l2.classHasListQuestion.createdAt;
+
+      return date1 > date2 ? 1 : -1;
+    });
+
+    return lists;
   }
 }
 module.exports = new ListQuestionsService();
